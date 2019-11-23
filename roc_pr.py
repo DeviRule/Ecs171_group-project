9ea63265538bb529dc1d33847ab051a920225b32
Hulin Wang <hulwang@ucdavis.edu>
2019-11-22 22:32:43 -0800

    slight modification of the roc_pr utility

--- roc_pr.py
+++ roc_pr.py
@@ -2,10 +2,10 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score
 from sklearn.metrics import auc,roc_auc_score,roc_curve,precision_recall_curve
-
+#很关键  大家在用之前把 Y_observed 对应的dataframe 或者 list 转换成 numpy.ndarray 要不然会报错
+# model name 记得改
 def Draw_ROC(Y_prob, Y_observed, model_name = 'Model'):
     ns_probs = [0 for _ in range(len(Y_observed))]
-    Y_prob = Y_prob[:, 1]
     # calculate scores
     ns_auc = roc_auc_score(Y_observed, ns_probs)
     lr_auc = roc_auc_score(Y_observed, Y_prob)
@@ -19,6 +19,7 @@ def Draw_ROC(Y_prob, Y_observed, model_name = 'Model'):
     plt.plot(ns_fpr, ns_tpr, linestyle='--', label='Chance')
     plt.plot(lr_fpr, lr_tpr, marker='.', label=model_name)
     # axis labels
+    plt.title('Receiver operating characteristic curve')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     # show the legend
@@ -27,7 +28,6 @@ def Draw_ROC(Y_prob, Y_observed, model_name = 'Model'):
     plt.show()
 
 def Draw_PR(Y_prob, Y_predicted, Y_observed, model_name = 'Model'):
-    Y_prob = Y_prob[:, 1]
     # predict class values
     lr_precision, lr_recall, _ = precision_recall_curve(Y_observed, Y_prob, pos_label=1)
     lr_f1, lr_auc = f1_score(Y_observed, Y_predicted), auc(lr_recall, lr_precision)
@@ -38,6 +38,7 @@ def Draw_PR(Y_prob, Y_predicted, Y_observed, model_name = 'Model'):
     plt.plot([0, 1], [no_skill, no_skill], linestyle='--', label='Chance')
     plt.plot(lr_recall, lr_precision, marker='.', label=model_name)
     # axis labels
+    plt.title('2-class Precision-Recall curve')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     # show the legend
